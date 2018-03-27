6fd5ad77520cac6bbc056854bbf7953e7a9528c1
Greg <gregshamalta@yahoo.com>
2018-03-27 14:59:44 -0700

    Issue #1 First Genres Component

---
+++ genre-list/components/genre-list.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GenreList() {
+  return (
+    <div>
+      <h2>Find Your Enjoyment</h2>
+    </div>
+  )
+}
---
+++ genre-list/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GenreList from './components/genre-list'
+
+ReactDOM.render(
+  <GenreList />, document.querySelector('#app')
+)
