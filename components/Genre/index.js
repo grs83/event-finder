2215af3a449d75ec79b9564634c3863e18248382
Greg <gregshamalta@yahoo.com>
2018-03-29 13:50:07 -0700

    Add CSS from Semantic library completed issue #1

--- components/Genre/index.js
+++ components/Genre/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 export default function Genre({ category: { img, title } }) {
   return (
-    <div className="ui cards">
-      <div className="ui fluid card">
-        <img src={img} />
-        <h3>{title}</h3>
+    <div className="card">
+      <div className="ui segment">
+        <img className="ui fluid image" src={img} />
+        <div
+          className="ui top aligned set active dimmer show"
+          style={{ backgroundColor: 'rgba(75,75,75,0.6)' }}
+        >
+          <div className="content">
+            <h2
+              className="ui large top center aligned header"
+              style={{ letterSpacing: '5px' }}
+            >
+              {title}
+            </h2>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/Genres/index.js
+++ components/Genres/index.js
@@ -4,51 +4,16 @@ import Genre from '../Genre';
 export default function Genres({ genresCategories }) {
   return (
     <div>
-      <h2>Find Your Enjoyment</h2>
-      <div className="ui two column centered grid">
-        <div className="four column centered row">
-          {genresCategories.reduce(function(genres, category, index) {
-            console.log(index);
-          }, [])}
-          <div className="column">
-            <div className="ui card fluid">
-              <div className="content">
-                <div className="header">This</div>
-              </div>
-            </div>
-          </div>
-          <div className="column">
-            <div className="ui card fluid">
-              <div className="content">
-                <div className="header">This</div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="four column centered row">
-          <div className="column">
-            <div className="ui card fluid">
-              <div className="content">
-                <div className="header">This</div>
-              </div>
-            </div>
-          </div>
-          <div className="column">
-            <div className="ui card fluid">
-              <div className="content">
-                <div className="header">This</div>
-              </div>
-            </div>
+      <h2 className="ui center aligned huge header">Find Your Enjoyment</h2>
+      <div className="ui one column doubling stackable grid container">
+        <div className="column">
+          <div className="ui two stackable cards">
+            {genresCategories.map((category, i) => {
+              return <Genre key={i} category={category} />;
+            })}
           </div>
         </div>
       </div>
     </div>
   );
 }
-
-// {
-//   genresCategories.map((category, i) => {
-//     return <Genre category={category} />;
-//   });
-// }
